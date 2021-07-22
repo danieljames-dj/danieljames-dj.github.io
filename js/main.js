8f49f9e8d2972beadc4bd6768a1994613892a67c
Daniel James <djdany444@gmail.com>
2021-07-22 17:27:08 +0530

    revamped website a little bit

--- js/main.js
+++ js/main.js
@@ -9,6 +9,98 @@
 
 	"use strict";
 
+	window.addEventListener('load', () => {
+		AOS.init({
+			duration: 1000,
+			easing: "ease-in-out",
+			once: true,
+			mirror: false
+		});
+	});
+
+	const select = (el, all = false) => {
+		el = el.trim()
+		if (all) {
+			return [...document.querySelectorAll(el)]
+		} else {
+			return document.querySelector(el)
+		}
+	}
+
+	const on = (type, el, listener, all = false) => {
+		let selectEl = select(el, all)
+		if (selectEl) {
+			if (all) {
+			selectEl.forEach(e => e.addEventListener(type, listener))
+			} else {
+			selectEl.addEventListener(type, listener)
+			}
+		}
+	}
+
+	  /**
+   * Porfolio isotope and filter
+   */
+	window.addEventListener('load', () => {
+	// let portfolioContainer = select('.portfolio-container');
+	// if (portfolioContainer) {
+	// 	let portfolioIsotope = new Isotope(portfolioContainer, {
+	// 	itemSelector: '.portfolio-item'
+	// 	});
+
+	// 	let portfolioFilters = select('#portfolio-flters li', true);
+
+	// 	on('click', '#portfolio-flters li', function(e) {
+	// 	e.preventDefault();
+	// 	portfolioFilters.forEach(function(el) {
+	// 		el.classList.remove('filter-active');
+	// 	});
+	// 	this.classList.add('filter-active');
+
+	// 	portfolioIsotope.arrange({
+	// 		filter: this.getAttribute('data-filter')
+	// 	});
+	// 	portfolioIsotope.on('arrangeComplete', function() {
+	// 		AOS.refresh()
+	// 	});
+	// 	}, true);
+	// }
+
+	});
+
+	/**
+	 * Initiate portfolio lightbox 
+	 */
+	const portfolioLightbox = GLightbox({
+	selector: '.portfolio-lightbox'
+	});
+
+	/**
+	 * Initiate portfolio details lightbox 
+	 */
+	const portfolioDetailsLightbox = GLightbox({
+	selector: '.portfolio-details-lightbox',
+	width: '90%',
+	height: '90vh'
+	});
+
+	/**
+	 * Portfolio details slider
+	 */
+	new Swiper('.portfolio-details-slider', {
+	speed: 400,
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	}
+	});
+
 	/*---------------------------------------------------- */
 	/* Preloader
 	------------------------------------------------------ */ 
