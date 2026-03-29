f68bd095b4bd0e3cfcfb4a929566a037e5b10410
Daniel James <djdany444@gmail.com>
2026-03-30 00:30:18 +0530

    Add about page

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <section id="about" className="section about-section">
+      <div className="row section-intro">
+        <div className="col-twelve">
+          <h5>About Me</h5>
+          <h1>Let me introduce myself.</h1>
+          <p className="lead">To be updated.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
